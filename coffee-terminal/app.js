b6c429679418149383041eb885fbb161ff79e694
Vibes Terminal <vibes.terminal@gmail.com>
2025-12-09 17:08:36 +0200

    uzduotis i

--- coffee-terminal/app.js
+++ coffee-terminal/app.js
@@ -1,10 +1,37 @@
-// Sukurti mygtuką, kurį paspaudus jo spalva keičiasi į atsitiktine spalvą;
-// Sukurti mygtuką, kurį paspaudus jo forma pasikeičia į apskritimą, o paspaudus dar kartą grįžta į pradinę formą;
-// Sukurti du mygtukus vieną su tekstu “X”, kitą su tekstu “O”. Paspaudus ant bet kurio mygtuko abiejų mygtukų tekstai susikeičia;
-// Sukurti vieną tekstininį input elementą ir vieną div elementą. Rašant tekstą į input elementą, tas pats tekstas atsikartojamas ir div elemente;
-// Sukurti du tekstinius input elementus ir vieną div elementą. Div elemente rodomas tekstas “Įveskite skaičius”. Jeigu abiejuose input elementuose įvesti skaičiai, div tekstas pasikeičia į tų skaičių sumą. Visai kitais atvejais (tuščias, ne skaičius) rodomas tas pats tekstas “Įveskite skaičius”;
-// Sukurti range tipo input elementą. Nustatyti intervalą nuo 10 iki 99 (žingsnis 1). Sukurti div elementą, kuriame būtų atvaizduojama rage tipo elemento reikšmė (keičiant range slankiklio padėtį reikšmė turi taip pat atitinkamai keistis);
-// Nupaišyti kvadratą su bet kokiu tekstu viduje. Šalia sukurti du color tipo input elementus. Vienas kvadrato spalvai, kitas teksto spalvai nustatyti. Keičiant input elementų reikšmes atitinkamai keičiasi kvadrato ir teksto spalvos;
-// Sukurti text tipo input laukelį, mygtuką “Pridėti” ir tuščią sąrašą <ol>. Į input laukelį įrašius žodį ir paspaudus “Pridėti”, žodis turi atsirasti sąraše. Sąrašas visada turi būti išrūšiuotas paga abėcėlę;
-//  Iš div elementų sukurti tris skirtingų spalvų kvadratus. Kvadratai turi būti sudėti vienas į kitą (<div><div><div></div></div></div>) Paspaudus ant konkretaus kvadrato, šalia esančiam dar viename bendrame div elemente turi atsirasti to kvadrato spalva (viso turi būti trys spalvos, nekintančios)
-// Sukurti color tipo input laukelį ir div elementą. Parinkus input laukelyje spalvą, tos spalvos vardas turi atsirasti div elemente. Spalvų vardus imti iš API https://www.thecolorapi.com/ Dokumentacija nuorodoje.
+//  Iš div elementų sukurti tris skirtingų spalvų kvadratus.
+//  Kvadratai turi būti sudėti vienas į kitą (<div><div><div></div></div></div>)
+// Paspaudus ant konkretaus kvadrato, šalia esančiam dar viename bendrame div elemente turi atsirasti to kvadrato spalva (viso turi būti trys spalvos, nekintančios)
+
+const intoElement1 = document.querySelector('.i_TrysKvadratai div');
+intoElement1.style.width = 300 + 'px';
+intoElement1.style.height = 300 + 'px';
+intoElement1.style.backgroundColor = 'red';
+
+const intoElement2 = document.querySelector('.i_TrysKvadratai div>div');
+intoElement2.style.width = 200 + 'px';
+intoElement2.style.height = 200 + 'px';
+intoElement2.style.backgroundColor = 'blue';
+
+const intoElement3 = document.querySelector('.i_TrysKvadratai div>div>div');
+intoElement3.style.width = 100 + 'px';
+intoElement3.style.height = 100 + 'px';
+intoElement3.style.backgroundColor = 'orange';
+
+const iSpalva = document.querySelector('.i_Spalva');
+iSpalva.style.width = 100 + 'px';
+iSpalva.style.height = 100 + 'px';
+iSpalva.style.backgroundColor = 'gray';
+
+intoElement1.addEventListener('click', (e) => {
+    e.stopPropagation();
+    iSpalva.style.backgroundColor = intoElement1.style.backgroundColor;
+});
+intoElement2.addEventListener('click', (e) => {
+    e.stopPropagation();
+    iSpalva.style.backgroundColor = intoElement2.style.backgroundColor;
+});
+
+intoElement3.addEventListener('click', (e) => {
+    e.stopPropagation();
+    iSpalva.style.backgroundColor = intoElement3.style.backgroundColor;
+});
