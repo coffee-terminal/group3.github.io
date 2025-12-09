ab32257aeabf9297be2865a8cfbb820b057218b2
Vibes Terminal <vibes.terminal@gmail.com>
2025-12-09 21:47:30 +0200

    uzduotis j

--- coffee-terminal/app.js
+++ coffee-terminal/app.js
@@ -35,3 +35,32 @@ intoElement3.addEventListener('click', (e) => {
     e.stopPropagation();
     iSpalva.style.backgroundColor = intoElement3.style.backgroundColor;
 });
+
+// Sukurti color tipo input laukelį ir div elementą. Parinkus input laukelyje spalvą, tos spalvos vardas turi atsirasti div elemente. Spalvų vardus imti iš API https://www.thecolorapi.com/ Dokumentacija nuorodoje.
+
+const colorPicker = document.querySelector('#j_Picker');
+colorPicker.addEventListener('input', updateFirst);
+colorPicker.addEventListener('change', update);
+function updateFirst(event) {
+    const divColor = document.querySelector('.j_colorDiv');
+    if (divColor) {
+        divColor.style.backgroundColor = event.target.value;
+    }
+}
+function update(event) {
+    const divColor = document.querySelector('.j_colorDiv');
+    divColor.style.backgroundColor = event.target.value;
+    metaColor = event.target.value.slice(1);
+    console.log(metaColor);
+
+    const provideColor = (meta) => {
+        divColor.innerHTML = meta.name.value;
+    };
+
+    fetch('https://www.thecolorapi.com/id?hex=' + metaColor)
+        .then((res) => res.json())
+        .then((meta) => {
+            console.log(meta);
+            provideColor(meta);
+        });
+}
