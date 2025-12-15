86e4b513df55f5f30de237c45281ee1818c5537c
Pinepuff <j.sabaliauskyte@gmail.com>
2025-12-15 13:57:50 +0200

    Updatas antras

--- Pinepuff/app.js
+++ Pinepuff/app.js
@@ -1,48 +1,13 @@
 console.log('testuoju');
 
-// let theText = myTextInput.value;
-
-// let sukeistasMygtukas2 = document.querySelector('.cMygtukas2');
-// sukeistasMygtukas2.addEventListener('click', _ => {
-//     sukeistasMygtukas2.innerText = 'X'
-// });
-
-// let sukeistasMygtukas1 = document.querySelector('.cMygtukas1');
-// sukeistasMygtukas1.addEventListener('click', _ => {
-//     sukeistasMygtukas1.innerText = 'O'
-// });
-
-// KITAM KARTUI: buvo gera ideja daryti IF daryti kad ir po antro pasispaudimo apsikeistu tekstas
-//bet reikia if padaryti evento viduje ne apacioje
-
-// let sukeistasMygtukas2 = document.querySelector('.cMygtukas2');
-// sukeistasMygtukas2.addEventListener('click', _ => {
-//     sukeistasMygtukas2.innerText = 'X'
-// });
-
-// let sukeistasMygtukas1 = document.querySelector('.cMygtukas1');
-// sukeistasMygtukas1.addEventListener('click', _ => {
-//     if (sukeistasMygtukas1.innerText === 'X') {
-//         sukeistasMygtukas1.innerText = 'O'
-//     } else { sukeistasMygtukas1.innerText = 'X' }
-
-// });
-
-// let sukeistasMygtukas2 = document.querySelector('.cMygtukas2');
-// sukeistasMygtukas2.addEventListener('click', _ => {
-//     if (sukeistasMygtukas2.innerText === 'O') { sukeistasMygtukas2.innerText = 'X' }
-//     else { sukeistasMygtukas2.innerText = 'O' }
-
-// });
-
-let sukeistasMygtukas1 = document.querySelector('.cMygtukas1');
-let sukeistasMygtukas2 = document.querySelector('.cMygtukas2');
+let sukeistasMygtukas1 = document.querySelector('.c_Mygtukas1');
+let sukeistasMygtukas2 = document.querySelector('.c_Mygtukas2');
 
 function swapButtonText() {
-    let ctext1 = sukeistasMygtukas1.innerText;
-    let ctext2 = sukeistasMygtukas2.innerText;
-    sukeistasMygtukas1.innerText = ctext2;
-    sukeistasMygtukas2.innerText = ctext1;
+    let c_text1 = sukeistasMygtukas1.innerText;
+    let c_text2 = sukeistasMygtukas2.innerText;
+    sukeistasMygtukas1.innerText = c_text2;
+    sukeistasMygtukas2.innerText = c_text1;
 }
 
 sukeistasMygtukas1.addEventListener('click', swapButtonText);
@@ -50,8 +15,8 @@ sukeistasMygtukas2.addEventListener('click', swapButtonText);
 
 //dInputas dDivas
 
-let sukeistasInput = document.querySelector('.dInputas');
-let sukeistasDivas = document.querySelector('#values');
+let sukeistasInput = document.querySelector('.d_Inputas');
+let sukeistasDivas = document.querySelector('#d_values');
 //.getElementById("values");
 
 function repeatInputText(e) {
